f3f32d0a7193bc3e2cebbf5b7e1d413c11969f3e
Jay Nandwana <69632001+jaynandwana200@users.noreply.github.com>
2024-09-23 20:14:41 +0530

    hell

--- Django/MAC/MAC/urls.py
+++ Django/MAC/MAC/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/',include('shop.urls')),
+    path('',include('shop.urls')),
     path('blog/',include('blog.urls')),
 ]+ static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
