e302d8104035fa4f7f92f6b1f7468dfa6936e299
Jay Nandwana <69632001+jaynandwana200@users.noreply.github.com>
2024-09-25 19:52:27 +0530

    commit

--- Django/MAC/MAC/settings.py
+++ Django/MAC/MAC/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/shop/'
 
 #managing media
 MEDIA_URL = '/media/'
