2d5870f64c08ad5d555f1359e5074c9c3891c797
Jay Nandwana <69632001+jaynandwana200@users.noreply.github.com>
2024-12-31 23:55:24 +0530

    Update build_files.sh

--- Django/build_files.sh
+++ Django/build_files.sh
@@ -1,4 +1,5 @@
 echo "Build Start"
+python -m pip install django
 python -m pip install -r requirements.txt
 python manage.py manage.py makemigrations --noinput --clear
 python manage.py manage.py migrate --noinput --clear
